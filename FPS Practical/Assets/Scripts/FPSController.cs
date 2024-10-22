ba302a3dec034db0ceea39a8e3c4b789a9a2f86e
HiImKnurd <hiimknurd42wenty@gmail.com>
2024-10-22 11:39:29 +0800

    prac02

--- FPS Practical/Assets/Scripts/FPSController.cs
+++ FPS Practical/Assets/Scripts/FPSController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,8 +9,14 @@ public class FPSController : MonoBehaviour
     [Header("Movement Settings")]
     [SerializeField] private float _moveSpeed = 10f;
     [SerializeField] private float _jumpHeight = 10f;
-    [SerializeField] private float _sensitivity = 10f;
-    [SerializeField] private float _gravityScale = -9.81f;
+    [SerializeField] private float _verticalSensitivity = 10f;
+    [SerializeField] private float _horizontalSensitivity = 20f;
+    [SerializeField] private float _gravityScale = 1f;
+    [SerializeField] private float _currentSpeedMult = 1f;
+    [SerializeField] private float _sprintSpeedMult;
+    [SerializeField] private float _moveSpeedTransition = 2f;
+    [SerializeField] private float _normalHeight = 2f;
+    [SerializeField] private float _crouchHeight = 1f;
     [SerializeField] private PlayerInput _playerInput;
 
     private CharacterController _characterController;
@@ -17,9 +24,34 @@ public class FPSController : MonoBehaviour
     private InputAction _moveAction;
     private InputAction _lookAction;
     private InputAction _jumpAction;
+    private InputAction _sprintAction;
+    private InputAction _crouchAction;
+
     private Vector3 _move;
+    public float speed;
+    public bool crouching = false;
     private Vector3 _verticalVelocity = Vector3.zero;
+
     private Vector3 _mouseDelta;
+    private float cameraPitch = 0f;
+
+    [SerializeField] private float _coyoteTime = 0.2f;
+    private float coyoteTimeCounter;
+
+    private bool moving = false;
+    private float bobOffset;
+    [SerializeField] private float _bobFrequency;
+    [SerializeField] private float _bobAmplitude;
+    private float timer = 0f;
+    private float originalCamY;
+
+    private InputAction _shootAction;
+    private Vector3 originalPosition;
+    private Vector3 shakeOffset;
+    [SerializeField] private Vector3 _shakeStrength;
+    private float shakeDuration = 0.5f;
+    private float shakeTimer = 0f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -28,8 +60,15 @@ public class FPSController : MonoBehaviour
         _moveAction = _playerInput.actions["Move"];
         _lookAction = _playerInput.actions["Look"];
         _jumpAction = _playerInput.actions["Jump"];
+        _sprintAction = _playerInput.actions["Sprint"];
+        _crouchAction = _playerInput.actions["Crouch"];
+        _shootAction = _playerInput.actions["Shoot"];
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        speed = _moveSpeed;
+        _characterController.height = _normalHeight;
+        originalCamY = Camera.main.transform.position.y;
+        originalPosition = Camera.main.transform.position;
     }
 
     // Update is called once per frame
@@ -39,33 +78,115 @@ public class FPSController : MonoBehaviour
         
         Look();
         Jump();
+        Crouch();
+        Shoot();
 
         _move = transform.right * input.x + transform.forward * input.y;
+        _verticalVelocity.y += _gravityScale * -9.81f * Time.deltaTime;
 
-        _verticalVelocity.y += _gravityScale * Time.deltaTime;
-        Vector3 finalMove = _verticalVelocity + (_move * _moveSpeed);
+        if(_sprintAction.IsPressed() && !crouching) _currentSpeedMult = _sprintSpeedMult;
+        else _currentSpeedMult = 1f;
+
+        speed = Mathf.Lerp(speed, _moveSpeed * _currentSpeedMult, _moveSpeedTransition * Time.deltaTime);
+        if (_move != Vector3.zero && _characterController.isGrounded) moving = true;
+        else moving = false;
+
+        Vector3 finalMove = _verticalVelocity + (_move * speed);
 
         _characterController.Move(finalMove * Time.deltaTime);
+    }
+
+    private void LateUpdate()
+    {
+        HandleCameraPitch();
+        BobHandler();
+        HandleCameraShake();
+
+        Camera.main.transform.localPosition = originalPosition + shakeOffset + new Vector3(0f, bobOffset, 0f);
+    }
+
+    void HandleCameraPitch()
+    {
+        float mouseY = _mouseDelta.y * _verticalSensitivity * Time.deltaTime;
+
+        cameraPitch -= mouseY;
+        cameraPitch = Mathf.Clamp(cameraPitch, -90f, 90f);
+        Camera.main.transform.localRotation = Quaternion.Euler(cameraPitch, 0, 0);
+    }
+
+    void BobHandler()
+    {
+        if(moving)
+        {
+            timer += _bobFrequency * _currentSpeedMult * Time.deltaTime;
+            bobOffset = Mathf.Sin(timer) * _bobAmplitude;
+        }
+        else
+        {
+            bobOffset = Mathf.Lerp(bobOffset, 0, Time.deltaTime);
+        }
+
+        Transform camtransform = Camera.main.transform;
+        camtransform.localPosition = new Vector3(camtransform.localPosition.x, originalCamY + bobOffset, camtransform.localPosition.z);
+        //Camera.main.transform.localPosition = camtransform.localPosition;
+    }
+
+    void HandleCameraShake()
+    {
+        if(shakeTimer > 0)
+        {
+            shakeTimer -= Time.deltaTime;
+            shakeOffset = Random.insideUnitCircle * _shakeStrength;
+        }
+        else
+        {
+            shakeOffset = Vector3.zero;
+        }
     }
 
     void Look()
     {
         _mouseDelta = _lookAction.ReadValue<Vector2>();
 
-        float mouseX = _mouseDelta.x * _sensitivity * Time.deltaTime;
+        float mouseX = _mouseDelta.x * _horizontalSensitivity * Time.deltaTime;
         transform.Rotate(Vector3.up * mouseX);
     }
 
     void Jump()
     {
-        if(_jumpAction.IsPressed() && _characterController.isGrounded)
+        if (_characterController.isGrounded) coyoteTimeCounter = _coyoteTime;
+        else coyoteTimeCounter -= Time.deltaTime;
+
+        if (_jumpAction.IsPressed() && coyoteTimeCounter > 0f)
         {
-            _verticalVelocity.y = Mathf.Sqrt(-2 * _gravityScale * _jumpHeight);
+            _verticalVelocity.y = Mathf.Sqrt(-2 * -9.81f * _gravityScale * _jumpHeight);
         }
         
         if(_characterController.isGrounded && _verticalVelocity.y < 0)
         {
             _verticalVelocity.y = -1;
+        }
+    }
+    private void Crouch()
+    {
+        if (_crouchAction.IsPressed())
+        {
+            _characterController.height = _crouchHeight;
+            _characterController.center = new Vector3(0, _crouchHeight / 2, 0);
+            crouching = true;
+}
+        else
+        {
+            _characterController.height = _normalHeight;
+            _characterController.center = new Vector3(0, 0, 0);
+            crouching = false;
+        }
+    }
+    private void Shoot()
+    {
+        if (_shootAction.IsPressed())
+        {
+            shakeTimer = shakeDuration; 
         }
     }
 }
